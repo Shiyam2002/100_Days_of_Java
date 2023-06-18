32b06f9451950eb40f626538d7e735e8dd03ce86
Shiyam2002 <k.s.shiyam2002@gmail.com>
2023-06-18 20:17:34 +0530

    Day-92

---
+++ _100_days_of_java/BlaptopAction.java
@@ -0,0 +1,10 @@
+package _100_days_of_java;
+
+public interface BlaptopAction {
+	public String AddnewProduct(Blaptop_Company laptop);
+	public void Listing();
+	public void Update(String name);
+	public void deleting(String name);
+	public void Searching(String name);
+	public void Sorting(); 
+}
