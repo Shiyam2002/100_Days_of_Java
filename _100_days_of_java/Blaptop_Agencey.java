b0a6ea47f8e513f79e8f9122c5abc25574c4f090
Shiyam2002 <k.s.shiyam2002@gmail.com>
2023-06-23 20:24:45 +0530

    Day-94

--- _100_days_of_java/Blaptop_Agencey.java
+++ _100_days_of_java/Blaptop_Agencey.java
@@ -1,24 +1,27 @@
-package _100_days_of_java;
-
 public class Blaptop_Agencey implements BlaptopAction {	
-	Blaptop_Agencey[] model = new Blaptop_Agencey[5];
+	Blaptop_Company[] model = new Blaptop_Company[5];
 	public Blaptop_Agencey(Integer i, String Model, String color, Integer j, Integer k) {
-		model[0] = new Blaptop_Agencey(2347853,"Blaptop vers","Red",35999,512);
-		model[1] = new Blaptop_Agencey(5793237,"Blaptop modex","Green",45999,512);
-		model[2] = new Blaptop_Agencey(9735728,"Blaptop unir","Silver",38999,512);
-		model[3] = new Blaptop_Agencey(9444225,"Blaptop yeri","Blue",76999,512);
+		model[0] = new Blaptop_Company(2347853,"Blaptop vers","Red",35999,512);
+		model[1] = new Blaptop_Company(5793237,"Blaptop modex","Green",45999,512);
+		model[2] = new Blaptop_Company(9735728,"Blaptop unir","Silver",38999,512);
+		model[3] = new Blaptop_Company(9444225,"Blaptop yeri","Blue",76999,512);
 	}
 
 	@Override
 	public String AddnewProduct(Blaptop_Company laptop) {
-		
-		return null;
+		for(Integer index=0;index<model.length;index++) {
+			if(model[index]==null) {
+				model[index]=laptop;
+			}
+		}
+		return "New List is been Added";
 	}
 
 	@Override
 	public void Listing() {
-		
-		
+		for(Blaptop_Company print:model) {
+			System.out.println(print);
+		}
 	}
 
 	@Override
@@ -29,8 +32,13 @@ public class Blaptop_Agencey implements BlaptopAction {
 
 	@Override
 	public void deleting(String name) {
-		
-		
+		for(Integer index=0;index<model.length;index++) {
+			if(model[index].getModelName().equalsIgnoreCase(name)) {
+				model[index]=null;
+				System.out.println(model[index]+" has been removed");
+				break;
+			}
+		}
 	}
 
 	@Override
